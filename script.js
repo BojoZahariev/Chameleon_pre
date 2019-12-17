d2654ee42789563256ba1e2f663bc304a9731ebe
Bojo <bozhidar_zahariev@hotmail.com>
2019-12-17 14:48:32 +0000

    only 3 work in progress

--- script.js
+++ script.js
@@ -13,24 +13,24 @@ class Chameleon extends React.Component {
 
 const ColorFrame1 = props => {
   return (
-    <div className="blocks" style={props.style} onClick={props.onClick}>
-      <p>RED: {props.color}</p>
+    <div className="blocks" onClick={props.onClick}>
+      <p>{props.color}</p>
     </div>
   );
 };
 
 const ColorFrame2 = props => {
   return (
-    <div className="blocks" style={props.style} onClick={props.onClick}>
-      <p>GREEN: {props.color}</p>
+    <div className="blocks" onClick={props.onClick}>
+      <p>{props.color}</p>
     </div>
   );
 };
 
 const ColorFrame3 = props => {
   return (
-    <div className="blocks" style={props.style} onClick={props.onClick}>
-      <p>BLUE: {props.color}</p>
+    <div className="blocks" onClick={props.onClick}>
+      <p>{props.color}</p>
     </div>
   );
 };
@@ -38,7 +38,7 @@ const ColorFrame3 = props => {
 const ColorFrame4 = props => {
   return (
     <div className="blocks" style={props.style} onClick={props.onClick}>
-      <p>RED2: {props.color}</p>
+      <p>{props.color}</p>
     </div>
   );
 };
@@ -49,7 +49,6 @@ class Container extends React.Component {
     this.state = {
       color: 'red',
       colorArray: [],
-      wrongColorArray: [],
       clickedBlocks: []
     };
 
@@ -69,12 +68,10 @@ class Container extends React.Component {
 
   changeColor() {
     let currentRGB = this.colorRGB();
-    let wrongRGB = this.colorRGB();
 
     this.setState({
       color: 'rgb(' + currentRGB[0] + ',' + currentRGB[1] + ',' + currentRGB[2] + ')',
-      colorArray: currentRGB,
-      wrongColorArray: wrongRGB
+      colorArray: currentRGB
     });
   }
 
@@ -90,42 +87,17 @@ class Container extends React.Component {
     console.log('TCL: Container -> render -> this.state.clickedBlocks', this.state.clickedBlocks);
     console.log('TCL: Container -> render -> this.state.colorArray', this.state.colorArray);
 
+    const message = <p>Success</p>;
+
+    const block1 = <ColorFrame1 onClick={() => this.sayHello(this.state.colorArray[0])} color={this.state.colorArray[0]} />;
+
+    const block2 = <ColorFrame2 onClick={() => this.sayHello(this.state.colorArray[1])} color={this.state.colorArray[1]} />;
+
+    const block3 = <ColorFrame3 onClick={() => this.sayHello(this.state.colorArray[2])} color={this.state.colorArray[2]} />;
+
     function arraysEqual(a1, a2) {
       return JSON.stringify(a1) == JSON.stringify(a2);
     }
-    const message = <p>Success</p>;
-
-    const block1 = (
-      <ColorFrame1
-        onClick={() => this.sayHello(this.state.colorArray[0])}
-        style={{ backgroundColor: 'rgb(' + this.state.colorArray[0] + ',' + '00' + ',' + '00' + ')' }}
-        color={this.state.colorArray[0]}
-      />
-    );
-
-    const block2 = (
-      <ColorFrame2
-        onClick={() => this.sayHello(this.state.colorArray[1])}
-        style={{ backgroundColor: 'rgb(' + '00' + ',' + this.state.colorArray[1] + ',' + '00' + ')' }}
-        color={this.state.colorArray[1]}
-      />
-    );
-
-    const block3 = (
-      <ColorFrame3
-        onClick={() => this.sayHello(this.state.colorArray[2])}
-        style={{ backgroundColor: 'rgb(' + '00' + ',' + '00' + ',' + this.state.colorArray[2] + ')' }}
-        color={this.state.colorArray[2]}
-      />
-    );
-
-    const block4 = (
-      <ColorFrame4
-        onClick={() => this.sayHello(this.state.wrongColorArray[0])}
-        style={{ backgroundColor: 'rgb(' + this.state.wrongColorArray[0] + ',' + '00' + ',' + '00' + ')' }}
-        color={this.state.wrongColorArray[0]}
-      />
-    );
 
     function shuffle(a) {
       for (let i = a.length - 1; i > 0; i--) {
@@ -134,7 +106,7 @@ class Container extends React.Component {
       }
       return a;
     }
-    let blocksArray = [block1, block2, block3, block4];
+    let blocksArray = [block1, block2, block3];
     shuffle(blocksArray);
 
     return (
@@ -146,17 +118,18 @@ class Container extends React.Component {
           {blocksArray[1]}
           {blocksArray[2]}
           {blocksArray[3]}
-          {blocksArray[4]}
         </div>
 
         <button className="testButton" onClick={this.changeColor}>
           START
         </button>
 
-        <div className="colorBar">
-          <p style={{ backgroundColor: 'rgb(' + this.state.clickedBlocks[0] + ',' + '00' + ',' + '00' + ')' }}>{this.state.clickedBlocks[0]}</p>
-          <p style={{ backgroundColor: 'rgb(' + '00' + ',' + this.state.clickedBlocks[1] + ',' + '00' + ')' }}>{this.state.clickedBlocks[1]}</p>
-          <p style={{ backgroundColor: 'rgb(' + '00' + ',' + '00' + ',' + this.state.clickedBlocks[2] + ')' }}>{this.state.clickedBlocks[2]}</p>
+        <div
+          className="colorBar"
+          style={{ backgroundColor: 'rgb(' + this.state.clickedBlocks[0] + ',' + this.state.clickedBlocks[1] + ',' + this.state.clickedBlocks[2] + ')' }}>
+          <p>{this.state.clickedBlocks[0]}</p>
+          <p>{this.state.clickedBlocks[1]}</p>
+          <p>{this.state.clickedBlocks[2]}</p>
         </div>
 
         <div style={{ backgroundColor: 'rgb(' + this.state.clickedBlocks[0] + ',' + this.state.clickedBlocks[1] + ',' + this.state.clickedBlocks[2] + ')' }}>
