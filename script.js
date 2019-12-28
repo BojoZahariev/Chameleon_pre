a063520fd3b3615ea0bbf8b778f738ddbfaae3ff
Bojo <bozhidar_zahariev@hotmail.com>
2019-12-28 08:05:10 +0000

    =

--- script.js
+++ script.js
@@ -367,7 +367,7 @@ class Container extends React.Component {
 
   //CHECKS IF IT IS THE RIGHT ORDER
   arraysEqual = (a1, a2) => {
-    return JSON.stringify(a1) == JSON.stringify(a2);
+    return JSON.stringify(a1) === JSON.stringify(a2);
   };
 
   render() {
